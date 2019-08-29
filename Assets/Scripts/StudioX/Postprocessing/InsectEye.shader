da3d3e9dab2e370d0041c5af33e8c71af1a00b41
jromphf <jromphf@library.rochester.edu>
2019-08-29 17:16:51 -0400

    postprocessing working with cardboard

--- Assets/Scripts/StudioX/Postprocessing/InsectEye.shader
+++ Assets/Scripts/StudioX/Postprocessing/InsectEye.shader
@@ -6,11 +6,12 @@ Shader "Hidden/Custom/InsectEye"
         #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
 
         TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-        float _Distortion;
+        float _Blend;
         float _UVTile = 10.0;
         float _HexSeparation = 0.05;
         float _HexAspect = sqrt(3.0);
-        
+        float4 _HexColor = float4(1.0, 1.0, 1.0, 1.0);
+
         float Hex(float2 uv)
         {
             float2 u = 8 * uv/_ScreenParams.x;
@@ -20,11 +21,20 @@ Shader "Hidden/Custom/InsectEye"
             return 0.5 * min(dot(a,a),dot(b,b));
         }
 
+        /*float2 Distort(float2 uv)
+        {
+            float aspect = _ScreenParams.x / _ScreenParams.y;
+            float strength = sin(1.0) * 2;
+            float2 intensity = float2(strength * aspect, strength * aspect);
+            float2 coords = (uv - 0.5) * 2.0;
+            return uv - float2((1.0 - coords.y * coords.y) * intensity.y * (coords.x), (1.0 - coords.x * coords.x) * intensity.x * (coords.y));
+        }*/
+
         float4 Frag(VaryingsDefault i): SV_Target
         {
             float hex = Hex(i.vertex.xy) * 0.5;
             float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
-            return lerp(color, hex, 0.3);
+            return lerp(color, _HexColor, hex * _Blend);
         }
     ENDHLSL
     SubShader
--- Assets/Scripts/StudioX/Postprocessing/InsectEyeEffect.cs
+++ Assets/Scripts/StudioX/Postprocessing/InsectEyeEffect.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 namespace StudioX
 {
     namespace Effects
@@ -9,8 +9,11 @@ namespace StudioX
         [PostProcess(typeof(InsectEyeEffectRenderer), PostProcessEvent.BeforeStack, "Custom/InsectEye")]
         public sealed class InsectEyeEffect : PostProcessEffectSettings
         {
-            [Range(0f, 25f), Tooltip("Distortion intensity")]
-            public FloatParameter distortion = new FloatParameter { value = 1.0f };
+            [Range(0f, 1.0f), Tooltip("Blend intensity")]
+            public FloatParameter blend = new FloatParameter { value = 0.25f };
+
+            [Range(0f, 1.0f), Tooltip("Hexagon Mask Color")]
+            public ColorParameter hexColor = new ColorParameter { value = new Color(1.0f, 1.0f, 1.0f, 1.0f) };
         }
 
         public sealed class InsectEyeEffectRenderer : PostProcessEffectRenderer<InsectEyeEffect>
@@ -18,7 +21,8 @@ namespace StudioX
             public override void Render(PostProcessRenderContext context)
             {
                 var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/InsectEye"));
-                sheet.properties.SetFloat("_Distortion", settings.distortion);
+                sheet.properties.SetFloat("_Blend", settings.blend);
+                sheet.properties.SetColor("_HexColor", settings.hexColor);
                 context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
             }
         }
