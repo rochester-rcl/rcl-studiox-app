f95f1b1dffbe9fc481dd999a4ae9e6ba16ed8f1a
jromphf <jromphf@library.rochester.edu>
2019-09-04 10:33:27 -0400

    finished basic screen recorder

--- Assets/Scripts/StudioX/ScreenRecorder/Simple360Capture.cs
+++ Assets/Scripts/StudioX/ScreenRecorder/Simple360Capture.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 namespace StudioX
 {
@@ -10,30 +12,63 @@ namespace StudioX
         public class Simple360Capture : MonoBehaviour
         {
             private Cubemap tex;
-            public Camera cam;
             public int cubemapSize = 512;
             public int targetFramerate = 30;
             public int duration;
             public bool startCaptureOnStart = true;
             public string imagePrefix;
             private Texture2D OutTex { get; set; }
-            private byte[] OutBuffer { get; set; }
             private CubemapFace[] CubemapFaces = { CubemapFace.PositiveX, CubemapFace.NegativeX,
             CubemapFace.PositiveY, CubemapFace.NegativeY,
             CubemapFace.PositiveZ, CubemapFace.NegativeZ };
             private int framesCaptured;
             private int totalFrames;
             private string imagesDir;
+            private Camera cam;
+            private GameObject progress;
+            private Text progressText;
+            private bool recording;
             void Start()
             {
+                cam = gameObject.AddComponent<Camera>();
+                cam.CopyFrom(Camera.main);
+                // IGNORE UI LAYER
+                cam.cullingMask = ~(1 << 5);
                 framesCaptured = 0;
                 totalFrames = duration * targetFramerate;
                 Application.targetFrameRate = targetFramerate;
                 imagesDir = FindRecordingsDir();
-                tex = new Cubemap(512, TextureFormat.RGBA32, false);
+                tex = new Cubemap(cubemapSize, TextureFormat.RGBA32, false);
                 tex.anisoLevel = 9;
                 OutTex = new Texture2D(tex.width, tex.height * 6, TextureFormat.RGBA32, false);
-                StartCoroutine(ExecRecording());
+                InitCanvas();
+                if (startCaptureOnStart)
+                {
+                    recording = true;
+                    StartCoroutine(ExecRecording());
+                }
+
+            }
+
+            private void InitCanvas()
+            {
+                progress = new GameObject();
+                progress.layer = 5;
+                progress.name = "ProgressOverlay";
+                Canvas canvas = progress.AddComponent<Canvas>();
+                CanvasScaler scaler = progress.AddComponent<CanvasScaler>();
+                progressText = progress.AddComponent<Text>();
+                progressText.alignment = TextAnchor.LowerCenter;
+                progressText.text = string.Format("Recorded {0} Frames / {1}", framesCaptured, totalFrames);
+                progressText.resizeTextMinSize = 14;
+                progressText.resizeTextMaxSize = 30;
+                progressText.resizeTextForBestFit = true;
+                progressText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+                scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+                scaler.matchWidthOrHeight = 0;
+                scaler.referencePixelsPerUnit = 100;
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                progress.SetActive(false);
             }
 
             private string FindRecordingsDir()
@@ -67,11 +102,16 @@ namespace StudioX
 
             private IEnumerator ExecRecording()
             {
-                while (framesCaptured < totalFrames)
+                recording = true;
+                progress.SetActive(true);
+                while (framesCaptured < totalFrames && recording)
                 {
                     yield return new WaitForEndOfFrame();
                     Capture();
                 }
+                recording = false;
+                framesCaptured = 0;
+                progress.SetActive(false);
                 Debug.Log("DONE RECORDING");
             }
 
@@ -80,10 +120,25 @@ namespace StudioX
                 if (cam)
                 {
                     CubemapToBitmapLayout();
-                    OutBuffer = OutTex.EncodeToPNG();
+                    byte[] OutBuffer = OutTex.EncodeToPNG();
                     string outPath = string.Format("{0}/{1}{2:D6}.png", imagesDir, imagePrefix, framesCaptured);
-                    System.IO.File.WriteAllBytes(outPath, OutBuffer);
+                    WriteFileAsync(OutBuffer, outPath);
                     framesCaptured++;
+                    progressText.text = string.Format("Recorded {0} Frames / {1}", framesCaptured, totalFrames);
+                }
+            }
+
+            private async void WriteFileAsync(byte[] buffer, string outPath)
+            {
+                string parentDir = Path.GetDirectoryName(outPath);
+                if (!Directory.Exists(Path.GetDirectoryName(outPath)))
+                {
+                    Directory.CreateDirectory(parentDir);
+                }
+                using (FileStream fs = File.Open(outPath, FileMode.Create))
+                {
+                    fs.Seek(0, SeekOrigin.End);
+                    await fs.WriteAsync(buffer, 0, buffer.Length);
                 }
             }
 
@@ -93,7 +148,15 @@ namespace StudioX
                 {
                     if (Input.GetKeyDown("space"))
                     {
-                        StartCoroutine(ExecRecording());
+                        if (recording)
+                        {
+                            recording = false;
+                        }
+                        else
+                        {
+                            StartCoroutine(ExecRecording());
+                        }
+
                     }
                 }
             }
