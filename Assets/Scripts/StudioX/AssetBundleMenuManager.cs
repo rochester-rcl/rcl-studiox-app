3458814ae696b64d8336be11d83540891822ddcf
jromphf <jromphf@library.rochester.edu>
2019-09-06 16:19:09 -0400

    working on loading menu items from bundles

---
+++ Assets/Scripts/StudioX/AssetBundleMenuManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StudioX
+{
+    public class AssetBundleMenuManager : MonoBehaviour
+    {
+        public AssetBundle[] Bundles { get; set; }
+        public GameObject menuPortrait;
+        public GameObject menuLandscape;
+        private int currentMenuId;
+        public enum MenuOrientation { Lanscape, Portrait };
+        private static readonly object managerLock = new object();
+        private static AssetBundleMenuManager _instance;
+        private List<GameObject> cells;
+        // TODO figure out how best to load display images from the bundle as well as their names
+
+        ///<summary>Static thread-safe singleton instance of AssetBundleMenuManager</summary>
+        public static AssetBundleMenuManager Instance
+        {
+            get
+            {
+                lock (managerLock)
+                {
+                    return _instance;
+                }
+            }
+        }
+
+        public void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Debug.LogWarning("Another instance of AssetBundleMenuManager was found in the scene. Destroying the current instance.");
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        public void Start()
+        {
+            InitMenuLandscape();
+            InitMenuPortrait();
+        }
+
+        private IEnumerator InitCells()
+        {
+            foreach(AssetBundle bundle in Bundles)
+            {
+
+            }
+        }
+
+        private IEnumerator PopulateCellData(ref AssetBundle bundle)
+        {
+            Sprite displayImage bundle.LoadAsset<Sprite>("Assets/") 
+        }
+
+        // We may be able to get away with the same layout if we use a GridLayout ... 
+        private void InitMenuPortrait()
+        {
+            CheckMenuForCanvas(ref menuPortrait);
+            GridLayout layout = menuPortrait.AddComponent<GridLayout>();
+
+        }
+
+        private void InitMenuLandscape()
+        {
+            CheckMenuForCanvas(ref menuPortrait);
+        }
+
+        private void CheckMenuForCanvas(ref GameObject go)
+        {
+            if (!go.GetComponent<Canvas>())
+            {
+                throw new MissingComponentException(string.Format("A Canvas Component is required on menu {0}", go.name));
+            }
+        }
+
+        private int GetActiveMenuId()
+        {
+            return GetActiveMenu().GetInstanceID();
+        }
+
+        private ref GameObject GetActiveMenu()
+        {
+            if (menuPortrait.activeSelf)
+            {
+                return ref menuPortrait;
+            }
+            return ref menuLandscape;
+        }
+    }
+}
--- Assets/Scripts/StudioX/CanvasToggleButtonHandler.cs
+++ Assets/Scripts/StudioX/CanvasToggleButtonHandler.cs
@@ -2,35 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class CanvasToggleButtonHandler : MonoBehaviour
+namespace StudioX
 {
-    public GameObject targetCanvas;
-    public GameObject closeButtonObject;
-    public GameObject openButtonObject;
-    private Button closeButton;
-    private Button openButton;
-    // Start is called before the first frame update
-    void Start()
+    public class CanvasToggleButtonHandler : MonoBehaviour
     {
-        openButton = openButtonObject.GetComponent<Button>();
-        openButton.onClick.AddListener(OpenHandler);
-        closeButton = closeButtonObject.GetComponent<Button>();
-        closeButton.onClick.AddListener(CloseHandler);
-        targetCanvas.SetActive(false);
-        closeButtonObject.SetActive(false);
-    }
+        public GameObject targetCanvas;
+        public GameObject closeButtonObject;
+        public GameObject openButtonObject;
+        private Button closeButton;
+        private Button openButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+            openButton = openButtonObject.GetComponent<Button>();
+            openButton.onClick.AddListener(OpenHandler);
+            closeButton = closeButtonObject.GetComponent<Button>();
+            closeButton.onClick.AddListener(CloseHandler);
+            targetCanvas.SetActive(false);
+            closeButtonObject.SetActive(false);
+        }
 
-    public void OpenHandler()
-    {
-        targetCanvas.SetActive(true);
-        openButtonObject.SetActive(false);
-        closeButtonObject.SetActive(true);
-    }
+        public void OpenHandler()
+        {
+            targetCanvas.SetActive(true);
+            openButtonObject.SetActive(false);
+            closeButtonObject.SetActive(true);
+        }
 
-    public void CloseHandler()
-    {
-        targetCanvas.SetActive(false);
-        openButtonObject.SetActive(true);
-        closeButtonObject.SetActive(false);
+        public void CloseHandler()
+        {
+            targetCanvas.SetActive(false);
+            openButtonObject.SetActive(true);
+            closeButtonObject.SetActive(false);
+        }
     }
 }
+
